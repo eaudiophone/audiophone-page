e6069abf8c60687cb42ab7008482208be974acd6
gabriel <gabmat1995@gmail.com>
2020-07-19 12:17:40 -0400

    wip: actualización de menu-responsive

--- components/navbar/navbar.php
+++ components/navbar/navbar.php
@@ -1,35 +1,53 @@
-<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light" id="cambio">
-	<img src="./img/Logo_01.png" class="logo-brand">
-	<button class="navbar-toggler" type="button" data-toggle="collapse" 
-		data-target="#menu-responsive" aria-controls="menu-responsive" 
-		aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="menu-responsive">
-		<div class="navbar-nav link-center" >
-			<a class="nav-item nav-link pr-5" href="#inicio">
-				<i class="fa fa-star pr-2"></i>
-				Inicio
-			</a>
-			<a class="nav-item nav-link pr-5" href="#servicios">
-				<i class="fa fa-microphone pr-2"></i>
-				Servicios
-			</a>
-			<a class="nav-item nav-link pr-5" href="#tarifas">
-				<i class="fa fa-money pr-2"></i>
-				Tarifas
-			</a>
-			<a class="nav-item nav-link" href="#contacto">
-				<i class="fa fa-home pr-2"></i>
-				Dirección
-			</a>
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg" id="cambio">
+
+	<div class="p-0 m-0 d-sm-none d-block w-100"> <!-- only-xs -->
+		<div class="p-0 m-0 d-flex fle-row justify-content-between align-items-center">
+			<img src="./img/Logo_01.png" class="logo-brand">
+			<span class="font-italic font-weight-bold" style="font-size: 13px;">Estudios Audiophone</span>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" 
+				data-target="#menu-responsive" aria-controls="menu-responsive" 
+				aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 		</div>
-		<!-- right side -->
-		<div class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
-			<a href="" class="btn dark-blue" data-toggle="modal" data-target="#modalContact">
-				<i class="fa fa-phone pr-1"></i>
-				Contáctenos
-			</a>
+	</div>
+
+	<div class="d-sm-none d-block w-100"> <!-- only-xs -->
+		<div class="collapse navbar-collapse" id="menu-responsive">
+			<div class="navbar-nav link-center" >
+				<a class="nav-item nav-link" href="#contacto">
+					<i class="fa fa-home"></i>
+					<span class="ml-3 font-menu-xs">Inicio</span>
+				</a>
+				<a class="nav-item nav-link" href="#servicios">
+					<i class="fa fa-microphone"></i>
+					<span class="ml-3 font-menu-xs">Estudio</span>
+				</a>
+				<a class="nav-item nav-link" href="#inicio">
+					<i class="fa fa-list-ul"></i>
+					<span class="ml-3 font-menu-xs">Servicios</span>
+				</a>
+				<a class="nav-item nav-link" href="#tarifas">
+					<i class="fa fa-map-marker"></i>
+					<span class="ml-3 font-menu-xs">Ubicación</span>
+				</a>
+				<a class="nav-item nav-link" href="#contacto">
+					<i class="fa fa-facebook"></i>
+					<span class="ml-3 font-menu-xs">Facebook</span>
+				</a>
+				<a class="nav-item nav-link" href="#contacto">
+					<i class="fa fa-instagram"></i>
+					<span class="ml-3 font-menu-xs">Instagram</span>
+				</a>
+				<a href="#" class="btn contact-button" data-toggle="modal" data-target="#modalContact">
+					<b class="font-weight-bold font-italic">Contáctar</b>
+				</a>
+			</div>
 		</div>
+	</div>
+
+	<div class="p-0 m-0 d-sm-block d-none" style="width: 20vw;"> <!-- others devices -->
+		<img src="./img/Logo_01.png" class="logo-brand">
+		<span class="font-italic font-weight-bold font-20 ml-2">Estudios Audiophone</span>
 	</div>
 </nav>
