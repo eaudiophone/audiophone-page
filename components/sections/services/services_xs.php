09055fb9bc103ceb7ff7dc23852a9344be2cdab6
gabriel <gabmat1995@gmail.com>
2020-08-06 18:53:51 -0400

    wip: ajuste de servicios

--- components/sections/services/services_xs.php
+++ components/sections/services/services_xs.php
@@ -1,3 +1,48 @@
-<div class="consola-xs mb-2"></div>
-<div class="alquiler-xs mb-2"></div>
-<div class="piano-xs mb-2"></div>
+<div class="consola-xs mb-2 effect">
+	<div class="w-100 h-100 d-flex flex-column align-items-center justify-content-center"
+		style="margin-left: 15vw;" 
+	>
+		<h4 class="font-italic mb-3 title"><b>Estudio de Grabación</b></h4>
+			<ul class="p-0 services-list" style="list-style: none;">
+				<li>Grabaciones</li>
+				<li>Mezclas</li>
+				<li>Masterizaciones</li>
+			</ul>
+
+			<button class="btn contact-button mt-3 button-shadow services-list" style="background-color: #343434">
+				<b class="font-weight-bold font-italic">Ver Descripción y Tarifas</b>
+			</button>
+	</div>
+</div>
+<div class="alquiler-xs mb-2 effect">
+	<div class="w-100 h-100 d-flex flex-column align-items-center justify-content-center"
+		style="margin-left: -9vw;" 
+	>
+		<h4 class="font-italic mb-3 title"><b>Alquiler de equipos</b></h4>
+		<ul class="p-0 services-list" style="list-style: none;">
+			<li>Transporte incluido.</li>
+			<li>Minitecas y Equipos.</li>
+			<li>Instrumentos</li>
+		</ul>
+
+		<button class="btn contact-button mt-3 button-shadow services-list" style="background-color: #343434">
+			<b class="font-weight-bold font-italic">Ver Descripción y Tarifas</b>
+		</button>
+	</div>
+</div>
+<div class="piano-xs mb-2 effect">
+	<div class="w-100 h-100 d-flex flex-column align-items-center justify-content-center"
+		style="margin-left: 15vw;" 
+	>
+		<h4 class="font-italic mb-3 title"><b>Grupo Músical Horizonte</b></h4>
+		<ul class="p-0 services-list" style="list-style: none;">
+			<li>Músicos profesionales</li>
+			<li>Servicio Personalizado</li>
+			<li>Sonido Incluido</li>
+		</ul>
+
+		<button class="btn contact-button mt-3 button-shadow services-list" style="background-color: #343434">
+			<b class="font-weight-bold font-italic">Ver Descripción y Tarifas</b>
+		</button>
+	</div>
+</div>
