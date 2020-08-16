7cb52833076967625a7508d16d6e6cb7c9e26d0c
gabriel <gabmat1995@gmail.com>
2020-08-15 20:30:24 -0400

    wip: ajustes responsive

--- components/modal/contact.php
+++ components/modal/contact.php
@@ -2,38 +2,32 @@
   aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-      <!--<div class="modal-header">
-        <h5 class="modal-title">
-          <i class="fa fa-phone pr-2"></i>
-          Contáctenos
-        </h5>
-      </div>  -->
-      <div class="modal-body p-0">
+      <div class="modal-body p-0" style="border-radius: 10px;">
         <div class="content w-100">
           <div class="row h-100 align-items-center">
-            <div class="col"></div>
-            <div class="col text-left">
+            <div class="col-sm-6 col-5"></div>
+            <div class="col-sm-6 col-7 text-left">
               <p>
+                <br>
                 Puede contactar con: <br><br>
-                Alfonso Martinez, Prod. Músical <br>
+                <span class="mr-2 font-italic">
+                  Alfonso Martínez
+                </span> <br>
+                Prod. Músical <br><br>
                 Telf. +58 416-9055-706 <br>
                 eaudiophonesa@gmail.com <br>
               </p>
-              <div class="w-100 d-flex justify-content-around flex-row">
+            </div>
+            <div class="col-sm-12">
+              <div class="w-100 d-flex flex-row justify-content-around">
                 <i class="fa fa-envelope fa-2x"></i>
                 <i class="fa fa-whatsapp fa-2x"></i>
-                <i  class="fa fa-instagram fa-2x"data-toggle="tooltip" data-placement="top"   title="estudios_audiophone"></i>
-              </div>
+                <i class="fa fa-instagram fa-2x"></i>
+              </div> 
             </div>
           </div>
         </div>
       </div>
-      <!-- <div class="modal-footer">
-        <button type="button" class="btn contact-button" data-dismiss="modal" style="font-style: italic;">
-          <i class="fa fa-close"></i>
-          <b>Cerrar</b>
-        </button>
-      </div> -->
     </div>
   </div>
 </div>
