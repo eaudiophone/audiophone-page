ea2aebb9389c2b304fce0fd31899be89e75d4959
gabmart1995 <gabmart1995@gmail.com>
2020-09-09 20:48:30 -0400

    wip: ajustes

--- components/sections/footer/footer_md_xl.php
+++ components/sections/footer/footer_md_xl.php
@@ -2,42 +2,44 @@
 
 function getFooter( $url ) {
 
-	return <<<TEMPLATE
-	<div class="d-flex flex-row justify-content-between" style="padding: 20px;">
-		<div class="font-italic">Design for: 
-			<a class="font-special" href="https://gabmart1995.github.io/" target="_blank">Gabriel Martinez</a> & 
-			<span class="font-special">Darianna Martinez</span>
-		</div>
-		<div class="links">
-			<a href="${url}">
-				<i class="fa fa-instagram fa-2x"></i>
-			</a>
-		</div>
-		<div class="terms" data-toggle="modal" data-target="#modalTerms">
-			Términos y condiciones del Servicio
-		</div>
-	</div>	
-	TEMPLATE;
+return <<<TEMPLATE
+<div class="d-flex flex-row justify-content-between" style="padding: 20px;">
+	<div class="font-italic">Design for: 
+		<a class="font-special" href="https://gabmart1995.github.io/" target="_blank">Gabriel Martinez</a> & 
+		<span class="font-special">Darianna Martinez</span>
+	</div>
+	<div class="links">
+		<a href="${url}">
+			<i class="fa fa-instagram fa-2x"></i>
+		</a>
+	</div>
+	<div class="terms" data-toggle="modal" data-target="#modalTerms">
+		Términos y condiciones del Servicio
+	</div>
+</div>	
+TEMPLATE;
+
 }
 
 function getFooterXS( $url ) {
 
-	return <<<TEMPLATE
-	<div class="d-flex flex-column justify-content-center align-items-center" style="padding: 20px;">
-		<div class="font-italic text-center mb-4">Design for: <br>
-			<a class="font-special" href="https://gabmart1995.github.io/" target="_blank">Gabriel Martinez</a> & 
-			<span class="font-special">Darianna Martinez</span>
-		</div>
-		<div class="links mb-4">
-			<a href="${url}">
-				<i class="fa fa-instagram fa-2x"></i>
-			</a>
-		</div>
-		<div class="terms" data-toggle="modal" data-target="#modalTerms">
-			Términos y condiciones del Servicio
-		</div>
+return <<<TEMPLATE
+<div class="d-flex flex-column justify-content-center align-items-center" style="padding: 20px;">
+	<div class="font-italic text-center mb-4">Design for: <br>
+		<a class="font-special" href="https://gabmart1995.github.io/" target="_blank">Gabriel Martinez</a> & 
+		<span class="font-special">Darianna Martinez</span>
 	</div>
-	TEMPLATE;
+	<div class="links mb-4">
+		<a href="${url}">
+			<i class="fa fa-instagram fa-2x"></i>
+		</a>
+	</div>
+	<div class="terms" data-toggle="modal" data-target="#modalTerms">
+		Términos y condiciones del Servicio
+	</div>
+</div>
+TEMPLATE;
+
 }
 
 ?>
