46a662d97e1f452f57a18fdedc34aa7fca200ac3
gabriel <gabmat1995@gmail.com>
2021-04-19 21:34:03 -0400

    wip: ajustes

--- components/modal/terms.php
+++ components/modal/terms.php
@@ -11,14 +11,14 @@
       <div class="modal-body">
         <p class="text-justify">
           La página web wwww.eaudiophone.com son plataformas de titularidad de Estudios Audiophone S.A.
-          sociedad venezolana con RIF "" y con residencia en la Av. Principal de Manicomio. Esq. Trinchera.
+          sociedad venezolana y con residencia en la Av. Principal de Manicomio. Esq. Trinchera.
           casa 152-10 Caracas Venezuela. <br> <br>
 
           Por favor, lee los presentes Términos de forma detenida de forma previa a la contratación de los Servicios. Al acceder, navegar y/o utilizar de cualquier forma la Web o los Servicios, reconoces haber leído, entendido y aceptado los presentes Términos en tú propio nombre o en nombre de la compañía u organización que representas. Asimismo, reconoces que has leído, entendido y aceptado quedar vinculado por los presentes Términos. En el supuesto que no estés de acuerdo con los presentes Términos, rogamos te abstengas de acceder, navegar o utilizar la Web, y los Servicios.
         </p>
 
         <h6 style="text-decoration: underline;"><b>Términos sobre el uso de la plataforma y servicios:</b></h6>
-        <ul class="modal-list text-justify">   
+        <ul class="modal-list text-justify">
           <li>
             Los acuerdos tendrán duración hasta que terminemos o suspendamos la relación por incumplimiento de las partes o Estudios Audiophone deje de prestar los servicios descritos.
           </li>
@@ -32,7 +32,7 @@
             El daño o hurto ocurrido durante el servicio de alquiler a equipos, instrumentos musicales y musicos por parte de cliente, será cobrado adicionalmente al presupuesto final. Sino cumple con el acuerdo, tiene el riesgo de demanda debido a la gravedad de la situación.
           </li>
           <li>
-            Para el servicio de Grabacíon estudio se cobra la tarifa por hora al final de la sesión, excepto que el cliente haya solicitado acceder a una produccion musical. 
+            Para el servicio de Grabacíon estudio se cobra la tarifa por hora al final de la sesión, excepto que el cliente haya solicitado acceder a una produccion musical.
           </li>
           <li>
             Las tarifas para el servicio de alquiler, contratación de músicos y producciones músicales sin excepción deben ser canceladas al 50% del presupuesto al momento de solicitar el servicio y el resto al cumplir los terminos del servicio solicitados con el cliente. Sin execepción.
@@ -41,7 +41,7 @@
             Tenga en cuenta si se realiza grabaciones afuera del estudio. Se le cobrará recargos adicionales.
           </li>
           <li>
-            Cualquier sugerencia, solicitud o reclamo dirigirse a los números que indican en la sección de Contáctanos. 
+            Cualquier sugerencia, solicitud o reclamo dirigirse a los números que indican en la sección de Contáctanos.
           </li>
           <li>
             Estudios Audiophone se reserva el derecho a modificar sus precios y planes en cualquier momento, cambio que será notificado por las plataformas asociadas al negocio.
